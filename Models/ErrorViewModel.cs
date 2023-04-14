9b1ab59410b37617ff74c45186e98a1802024161
Valentijn <theappleloaf@gmail.com>
2023-04-14 21:00:55 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Fishnice.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
